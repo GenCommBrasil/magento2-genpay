f735ed48d414de6a94fff54f135f361c9c8b23bc
Alex <alexsilva.info@gmail.com>
2019-06-24 11:59:28 -0300

    alterado o get conteudo do log para usar o Stream nos casos de arquivos grandes

--- Controller/Adminhtml/System/Config/DownloadLog.php
+++ Controller/Adminhtml/System/Config/DownloadLog.php
@@ -3,19 +3,11 @@
 namespace Rakuten\RakutenPay\Controller\Adminhtml\System\Config;
 
 use Magento\Backend\App\Action;
-use Magento\Backend\App\Action\Context;
 use Magento\Framework\App\Filesystem\DirectoryList;
-use Magento\Framework\Controller\Result\JsonFactory;
-use Magento\Framework\Filesystem\Io\File;
 
 class DownloadLog extends Action
 {
     CONST FILENAME = 'rakuten.log';
-
-    /**
-     * @var \Magento\Framework\Controller\Result\RawFactory
-     */
-    protected $resultRawFactory;
 
     /**
      * @var \Magento\Framework\Filesystem\DirectoryList
@@ -23,43 +15,46 @@ class DownloadLog extends Action
     protected $directoryList;
 
     /**
-     * @var \Magento\Framework\App\Response\Http\FileFactory
+     * @var \Magento\Framework\Filesystem
      */
-    protected $fileFactory;
+    protected $filesystem;
 
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\App\Response\Http\FileFactory $fileFactory,
-        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory,
-        \Magento\Framework\Filesystem\DirectoryList $directoryList
+        \Magento\Framework\Filesystem\DirectoryList $directoryList,
+        \Magento\Framework\Filesystem $filesystem
     ) {
         parent::__construct($context);
-        $this->fileFactory = $fileFactory;
-        $this->resultRawFactory = $resultRawFactory;
         $this->directoryList = $directoryList;
+        $this->filesystem = $filesystem;
     }
 
     public function execute()
     {
-        $resultRaw = $this->resultRawFactory->create();
-        $path = $this->directoryList->getPath('log');
-        $file = $path . DIRECTORY_SEPARATOR . self::FILENAME;
-        if (!file_exists($file)) {
-            $resultRaw->setHttpResponseCode(\Magento\Framework\Webapi\Exception::HTTP_NOT_FOUND);
+        $dir = $this->filesystem->getDirectoryWrite(DirectoryList::LOG);
+        if (!$dir->isFile(self::FILENAME)) {
+            $this->_response->setHttpResponseCode(\Magento\Framework\Webapi\Exception::HTTP_NOT_FOUND);
 
-            return $resultRaw;
+            return $this->_response;
         }
-        $content = file_get_contents($file);
 
-        $resultRaw->setHttpResponseCode(200)
-            ->setContents($content)
+        $contentLength = $dir->stat(self::FILENAME)['size'];
+        $this->_response->setHttpResponseCode(200)
             ->setHeader('Pragma', 'public', true)
             ->setHeader('Cache-Control', 'must-revalidate, post-check=0, pre-check=0', true)
             ->setHeader('Content-type', 'application/octet-stream', true)
-            ->setHeader('Content-Length', strlen($content), true)
+            ->setHeader('Content-Length', $contentLength)
             ->setHeader('Content-Disposition', 'attachment; filename="' . self::FILENAME . '"', true)
             ->setHeader('Last-Modified', date('r'), true);
+        $this->_response->sendHeaders();
 
-        return $resultRaw;
+        $stream = $dir->openFile(self::FILENAME, 'r');
+        while (!$stream->eof()) {
+            echo $stream->read(1024);
+        }
+        $stream->close();
+        flush();
+
+        return $this->_response;
     }
 }
